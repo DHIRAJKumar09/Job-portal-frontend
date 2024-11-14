d0fed8f05cae81240fdc7531d81f25d063152afd
Dhiraj kumar <55031476+DHIRAJKumar09@users.noreply.github.com>
2024-11-14 15:18:58 +0530

    Update FooterSection.js

--- src/Components/HomePage/FooterSection.js
+++ src/Components/HomePage/FooterSection.js
@@ -17,7 +17,9 @@ const Footer = () => {
   return (
     <Box
       sx={{
-        backgroundColor: '',
+           bgcolor: "#3f51b5"
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)"
         backdropFilter:'blur(10px)',
         color: 'white',
         fontWeight:'600',
